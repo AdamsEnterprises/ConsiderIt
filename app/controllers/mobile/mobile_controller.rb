08f85945afa23bc3966b86f8be39d1dceb9db522
Donovan Hunt <dhunt925@uw.edu>
2012-02-04 13:14:08 -0800

    Fixed up nav to match our new design (after Krysta's changes to the prototype

--- app/controllers/mobile/mobile_controller.rb
+++ app/controllers/mobile/mobile_controller.rb
@@ -9,21 +9,32 @@ class Mobile::MobileController < ApplicationController
   def option
     @option = Option.find_by_id(params[:option_id])
     @title = "#{@option.reference}"
+    # Determine if we have the forward button available since we also link to this page 
+    # from the nav and we don't want a forward option then (might be a buggy way to determine)
+    if (request.referrer == mobile_home_url)
+      @navigation = { :forward_path => mobile_option_initial_position_path(:option_id => @option.id) }
+    else
+      @navigation = {  }
+    end
   end
 
   def option_long_description
     @option = Option.find_by_id(params[:option_id])
     @title = "#{@option.reference}"
+    @navigation = {  }
   end
 
   def option_fiscal_impact
     @option = Option.find_by_id(params[:option_id])
     @title = "#{@option.reference}"
+    @navigation = {  }
   end
 
   def position_initial
     @option = Option.find_by_id(params[:option_id])
     @title = "#{@option.reference}"
+    @navigation = { :forward_path => mobile_option_points_path(:option_id => @option.id),
+                    :link_description => true, :option_id => @option.id }
   end
 
   def points

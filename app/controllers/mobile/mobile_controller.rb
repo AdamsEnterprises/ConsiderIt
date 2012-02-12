bc9ecd254e15d57850be7eaab61ad0f6d4151f4e
Donovan Hunt <dhunt925@uw.edu>
2012-02-12 12:31:48 -0800

    Added some navigation functionality to adding and removing points

--- app/controllers/mobile/mobile_controller.rb
+++ app/controllers/mobile/mobile_controller.rb
@@ -4,7 +4,6 @@ class Mobile::MobileController < ApplicationController
   
   # GET /mobile
   def index
-    @options = Option.all
   end
 
   # GET /mobile/options/:option_id
@@ -38,116 +37,6 @@ class Mobile::MobileController < ApplicationController
     else
       define_navigation
     end
-  end
-
-  # POST /mobile/options/:option_id/navigate
-  def navigate
-    option_id = params[:option_id]
-
-    if params[:button][:home]
-      # Going home.  Clear the navigation stack and go to the home side
-      session[option_id][:navigate].clear
-      redirect_path = mobile_home_path
-    elsif params[:button][:next]
-      # Next button.  Push onto stack and redirect
-      coming_from = params[:button][:next].keys.first
-      redirect_path = params[:button][:next][coming_from].keys.first
-      push_to_stack = true
-
-      if coming_from == mobile_option_initial_position_path(option_id)
-        # Next from choosing initial position.  Save in session data
-        if params[:position] and params[:position][:stance_bucket]
-          session[option_id][:position] = params[:position][:stance_bucket]
-        else
-          # Didn't input correct data.  Tell them that
-          flash[:error] = "No position chosen"
-          redirect_path = request.referrer
-          push_to_stack = false
-        end
-      elsif redirect_path == nil
-      end
-
-      if push_to_stack
-        # Push coming_from onto stack
-        session[option_id][:navigate].push coming_from
-      end
-    elsif params[:button][:initiative_description]
-      # Initiative Description button
-      redirect_path = show_mobile_option_path(option_id)
-
-      # Push old page onto stack
-      session[option_id][:navigate].push params[:button][:initiative_description].keys.first
-    elsif params[:button][:previous]
-      # Back button.  Pop off stack and go back
-      redirect_path = session[option_id][:navigate].pop
-    elsif params[:button][:description]
-      # Show long description pushed
-      redirect_path = show_mobile_option_long_description_path(params[:option_id])
-
-      # Push option overview path onto stack
-      session[option_id][:navigate].push show_mobile_option_path(option_id)
-    elsif params[:button][:fiscal_impact]
-      redirect_path = show_mobile_option_fiscal_impact_path(params[:option_id])
-
-      # Push option overview path onto stack
-      session[option_id][:navigate].push show_mobile_option_path(option_id)
-    elsif params[:button][:my_pros]
-      redirect_path = mobile_option_list_points_path(option_id, :pro)
-
-      # Push "my pros and cons" path onto stack
-      session[option_id][:navigate].push mobile_option_points_path(option_id)
-    elsif params[:button][:add_pros]
-      redirect_path = add_mobile_option_point_path(option_id, :pro)
-
-      # Push "my pros and cons" path onto stack
-      session[option_id][:navigate].push mobile_option_points_path(option_id)
-    elsif params[:button][:new_pro]
-      redirect_path = new_mobile_option_point_path(option_id, :pro)
-
-      # Push "my pros and cons" path onto stack
-      session[option_id][:navigate].push mobile_option_points_path(option_id)
-    elsif params[:button][:my_cons]
-      redirect_path = mobile_option_list_points_path(option_id, :con)
-
-      # Push "my pros and cons" path onto stack
-      session[option_id][:navigate].push mobile_option_points_path(option_id)
-    elsif params[:button][:add_cons]
-      redirect_path = add_mobile_option_point_path(option_id, :con)
-
-      # Push "my pros and cons" path onto stack
-      session[option_id][:navigate].push mobile_option_points_path(option_id)
-    elsif params[:button][:new_con]
-      redirect_path = new_mobile_option_point_path(option_id, :con)
-
-      # Push "my pros and cons" path onto stack
-      session[option_id][:navigate].push mobile_option_points_path(option_id)
-    elsif params[:button][:my_points]
-      redirect_path = mobile_option_points_path(option_id)
-
-      # Push "my pros and cons" path onto stack
-      session[option_id][:navigate].push mobile_option_summary_path(option_id)
-    elsif params[:button][:segment]
-      stance_bucket = params[:button][:segment].keys.first
-      
-      if params[:button][:segment][stance_bucket].is_a?(Hash) and
-         (params[:button][:segment][stance_bucket][:pro] or
-          params[:button][:segment][stance_bucket][:con])
-        # Going to the list of segments
-        redirect_path = mobile_option_segment_list_path(option_id, stance_bucket,
-                                                   params[:button][:segment][stance_bucket].keys.first)
-        # Push segment overview path onto stack
-        session[option_id][:navigate].push mobile_option_segment_path(option_id, stance_bucket)
-      else
-        # Going to segment overview
-        redirect_path = mobile_option_segment_path(option_id, stance_bucket)
-        # Push summary path onto stack
-        session[option_id][:navigate].push mobile_option_summary_path(option_id)
-      end
-    else
-      throw "No button action for " + params[:button].keys.first.to_s
-    end
-
-    redirect_to redirect_path
   end
 
   # GET /mobile/options/:option_id/description
--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,6 @@ ConsiderIt::Application.routes.draw do
 
   # mobile site
   match '/mobile' => "mobile/mobile#index", :via => :get, :module => :mobile, :as => :mobile_home
-  match '/mobile/options/:option_id/navigate' => "mobile/mobile#navigate", :module => :mobile, :via => :post, :as => :mobile_navigate
   match '/mobile/options/:option_id' => "mobile/mobile#option", :via => :get, :module => :mobile, :as => :show_mobile_option
   match '/mobile/options/:option_id/description' => "mobile/mobile#option_long_description", :via => :get, :module => :mobile, :as => :show_mobile_option_long_description
   match '/mobile/options/:option_id/fiscal_impact' => "mobile/mobile#option_fiscal_impact", :via => :get, :module => :mobile, :as => :show_mobile_option_fiscal_impact
@@ -49,4 +48,6 @@ ConsiderIt::Application.routes.draw do
   match '/mobile/options/:option_id/summary/:stance_bucket' => "mobile/mobile#segment", :via => :get, :module => :mobile, :as => :mobile_option_segment
   match '/mobile/options/:option_id/summary/:stance_bucket/:point_type' => "mobile/mobile#segment_list", :via => :get, :module => :mobile, :as => :mobile_option_segment_list
 
+  # Mobile Navigation
+  match '/mobile/options/:option_id/navigate' => "mobile/navigation#navigate", :module => :mobile, :via => :post, :as => :mobile_navigate
 end

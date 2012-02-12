cadc45fc5954dcb12dbe0ca3de2e1516e946db1e
Krysta Yousoufian <krystay@cs.washington.edu>
2012-02-12 16:59:22 -0500

    Adding mobile.js

---
+++ public/javascripts/mobile.js
@@ -0,0 +1,58 @@
+var hideClass = "hide";  // for hidden elements
+
+window.onload = function() {
+    /*** New point page ***/
+//    $("#add_link").click(addLink);
+    $j("#point_hide_name").click(warnHideName);
+    
+}
+/*
+function addLink() {
+  var fieldset = $("<fieldset />")
+    .append($("<div>")
+              .addClass("delete")
+              .addClass("clickable")
+              .click(deleteLink)
+              .text("delete"))
+    .append($("<input />")
+              .attr("type", "text")
+              .attr("size", "30")
+              .attr("id", "point_point_links_attributes_longcrazynumber_url")
+              .attr("title", "http://...")
+              .addClass("has_example")
+              .addClass("example")
+              .blur(showPlaceholder)
+              .focus(hidePlaceholder)
+              .blur())
+    .append($("<input />")
+              .attr("type", "text")
+              .attr("size", "30")
+              .attr("id", "point_point_links_attributes_longcrazynumber_description")
+              .attr("title", "A brief description")
+              .addClass("has_example")
+              .addClass("example")
+              .blur(showPlaceholder)
+              .focus(hidePlaceholder)
+              .blur());    
+  $("<div/>")
+    .addClass("point_link_form")
+    .append(fieldset)
+    .appendTo(".point_link_block");
+}
+*/
+function deleteLink() {
+  if (confirm("This link will be deleted.")) {
+    $j(this).closest(".point_link_form").remove();
+  }
+}
+
+function deletePoint() {
+    var del = confirm("This point will be removed from your list.\n\nDon't worry, though - if you change your mind later, you can always add it back.");
+
+}
+
+function warnHideName() {
+    if ($j(this).attr("checked"))
+        alert("We encourage you NOT to hide your name. Signing your point with your name " +
+            "lends it more weight to other LVG participants.");
+}
